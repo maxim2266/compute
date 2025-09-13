117609adc98c4a26a60b264212ea2c615a05e6d9
Maxim <maxim2266@outlook.com>
2025-09-13 11:44:59 +0100

    Another update to PHP code.

--- tools/setup.php
+++ tools/setup.php
@@ -1,20 +1,13 @@
 <?php
 # termination
 function fail(string $msg): never {
-	fwrite(STDERR, basename(__FILE__) . ": [ERROR] " . trim($msg) . "\n");
+	fwrite(STDERR, basename(__FILE__) . ": [ERROR] " . trim($msg) . PHP_EOL);
 	exit(1);
 }
 
 # number of functions
-if(!isset($argv[1]))
-	define("N", 10);
-elseif(is_numeric($argv[1])) {
-	define("N", (int)$argv[1]);
+define("N", isset($argv[1]) ? intval($argv[1]) : 10);
 
-	if(N < 3 || N > 32) {
-		fail("Parameter out of range: " . N);
-	}
-}
-else
-	fail("Invalid parameter \"$argv[1]\"");
+if(N < 3 || N > 20)
+	fail("Invalid parameter: \"{$argv[1]}\"");
 ?>
